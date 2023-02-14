3dee29cabfdc186624a5bf0f1ff3b597bcfdaafc
Anastasia <anyway98@mail.ru>
2023-02-14 17:58:21 +0300

    added pipe

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,75 +1,30 @@
-import hudson.tools.InstallSourceProperty
-import jenkins.model.Jenkins
-import jenkins.plugins.nodejs.tools.NodeJSInstallation
-import jenkins.plugins.nodejs.tools.NodeJSInstaller
-import static jenkins.plugins.nodejs.tools.NodeJSInstaller.DEFAULT_NPM_PACKAGES_REFRESH_HOURS
+pipeline {
+    agent any
 
-final versions = [
-        'NodeJS 8.x': '8.16.1'
-]
-
-Jenkins.instance.getDescriptor(NodeJSInstallation).with {
-    installations = versions.collect {
-        new NodeJSInstallation(it.key, null, [
-                new InstallSourceProperty([
-                        new NodeJSInstaller(it.value, null, DEFAULT_NPM_PACKAGES_REFRESH_HOURS)
-                ])
-        ])
-    }  as NodeJSInstallation[]
-}
-
-task_branch = "${TEST_BRANCH_NAME}"
-def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
-currentBuild.displayName = "$branch_cutted"
-base_git_url = "https://github.com/nstxoxo/UITestingPlayground-Cypress.git"
-node {
-    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
-        stage("Checkout Branch") {
-            if (!"$branch_cutted".contains("main")) {
-                try {
-                    getProject("$base_git_url", "$branch_cutted")
-                } catch (err) {
-                    echo "Failed get branch $branch_cutted"
-                    throw ("${err}")
+        stage('Build') {
+            steps {
+                nodejs(nodeJSInstallationName: 'Node 6.x', configId: '<config-file-provider-id>') {
+                    sh 'npm config ls'
                 }
-            } else {
-                echo "Current branch is main"
             }
         }
-
-        try {
-            runTest()
-        } finally {
-            stage ("Allure") {
-                generateAllure()
+        stage('Test') {
+            steps {
+                sh 'cypress:run'
+            }
+        }
+        stage('Report') {
+            steps {
+                script {
+                    allure([
+                    includeProperties: false,
+                    jdk: '',
+                    properties: [],
+                    reportBuildPolicy: 'ALWAYS',
+                    results: [[path: 'allure-results']]
+    ]) 
+    }
             }
         }
     }
-}
 
-
-def runTest() {
-    try {
-        script "npx cypress run --env allure=true"
-    } finally {
-        echo "some failed tests"
-    }
-}
-def getProject(String repo, String branch) {
-    cleanWs()
-    checkout scm: [
-            $class           : 'GitSCM', branches: [[name: branch]],
-            userRemoteConfigs: [[
-                                        url: repo
-                                ]]
-    ]
-}
-def generateAllure() {
-    allure([
-            includeProperties: true,
-            jdk              : '',
-            properties       : [],
-            reportBuildPolicy: 'ALWAYS',
-            results          : [[path: 'allure-results']]
-    ])
-}
