67dd10a7622d81d255168bf27487c06d68a7d03d
Anastasia <anyway98@mail.ru>
2023-02-14 12:36:05 +0300

    updated pipeline

--- jenkins.groovy
+++ jenkins.groovy
@@ -4,12 +4,13 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                echo 'Building..'
+                sh 'npm i'
             }
         }
         stage('Test') {
             steps {
-                echo 'Testing..'
+                sh 'npm run cypress:run'
+
             }
         }
         stage('Allure report') {
