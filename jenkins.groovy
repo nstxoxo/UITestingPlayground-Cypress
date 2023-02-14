ea2b9579215257c346af06e7a4fea8f5e859223d
Anastasia <anyway98@mail.ru>
2023-02-14 11:57:55 +0300

    updated pipeline

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,61 +1,26 @@
-task_branch = "${TEST_BRANCH_NAME}"
-def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
-currentBuild.displayName = "$branch_cutted"
-base_git_url = "https://github.com/nstxoxo/UITestingPlayground-Cypress.git"
+// task_branch = "${TEST_BRANCH_NAME}"
+// def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
+// currentBuild.displayName = "$branch_cutted"
+// base_git_url = "https://github.com/nstxoxo/UITestingPlayground-Cypress.git"
 
+pipeline {
+    agent any
 
-node {
-    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
-        stage("Checkout Branch") {
-            if (!"$branch_cutted".contains("main")) {
-                try {
-                    getProject("$base_git_url", "$branch_cutted")
-                } catch (err) {
-                    echo "Failed get branch $branch_cutted"
-                    throw ("${err}")
-                }
-            } else {
-                echo "Current branch is main"
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
             }
         }
-
-        try {
-            stage("Run tests") {
-                script "npx cypress run --env allure=true"
-        } finally {
-            stage ("Allure") {
-                generateAllure()
+        stage('Test') {
+            steps {
+                echo 'Testing..'
             }
         }
-       }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
     }
-}
-
-
-// def runTest() {
-//     try {
-//         script "npx cypress run --env allure=true"
-//     } finally {
-//         echo "some failed tests"
-//     }
-// }
-
-def getProject(String repo, String branch) {
-    cleanWs()
-    checkout scm: [
-            $class           : 'GitSCM', branches: [[name: branch]],
-            userRemoteConfigs: [[
-                                        url: repo
-                                ]]
-    ]
-}
-
-def generateAllure() {
-    allure([
-            includeProperties: true,
-            jdk              : '',
-            properties       : [],
-            reportBuildPolicy: 'ALWAYS',
-            results          : [[path: 'allure-results']]
-    ])
 }
